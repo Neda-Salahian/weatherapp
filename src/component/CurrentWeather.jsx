6f6219e44650808978110aa20741c1a6ad3866b7
TwinMine <michaelrolo11@gmail.com>
2024-01-17 13:27:29 +0100

    add background and icon

--- src/component/CurrentWeather.jsx
+++ src/component/CurrentWeather.jsx
@@ -11,8 +11,9 @@ const CurrentWeather = () => {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [error, setError] = useState(null);
-  const apiKey = 'd54a96e0b4517e304ec98021394e455b';
-  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  const apiKey = "d54a96e0b4517e304ec98021394e455b";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+  const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
     // Get the user's current location by default
@@ -24,7 +25,9 @@ const CurrentWeather = () => {
       },
       (error) => {
         console.error(`Error getting location: ${error.message}`);
-        setError('Error getting location. Please use the search to enter a city.');
+        setError(
+          "Error getting location. Please use the search to enter a city."
+        );
       }
     );
   }, []);
@@ -44,15 +47,15 @@ const CurrentWeather = () => {
       setError(null);
     } catch (error) {
       console.error(`Error in the request: ${error.message}`);
-      setError('Error fetching weather data.');
+      setError("Error fetching weather data.");
     }
   };
 
   const handleSearch = (city) => {
-    if (city.trim() !== '') {
+    if (city.trim() !== "") {
       fetchWeatherDataByCity(city);
     } else {
-      setError('Please enter a valid city name.');
+      setError("Please enter a valid city name.");
     }
   };
 
@@ -75,35 +78,52 @@ const CurrentWeather = () => {
       }
     } catch (error) {
       console.error(`Error in the request: ${error.message}`);
-      setError('Error fetching weather data.');
+      setError("Error fetching weather data.");
     }
   };
 
   // Bild für das jetzige Wetter
   const weatherBack = `url(${
-    data.find((item) => item.name === "moderate cloudy")?.pic
+    data.find((item) => item.name === catchWeather?.weather?.[0]?.description)
+      ?.pic
   })`;
   // Bild für das jetzige Wetter
 
+  console.log(catchWeather);
   return (
     <div className="display-weather" style={{ backgroundImage: weatherBack }}>
       <WeatherContext.Provider value={{ catchWeather, setCatchWeather }}>
         <div className="top-part">
           <h3>Willkommen {name}. Haben Sie einen schönen Tag</h3>
-
           {!catchWeather ? (
-            <p style={{ color: 'black' }}>{error || 'Loading....'}</p>
+            <p style={{ color: "black" }}>{error || "Loading...."}</p>
           ) : (
             <div>
               <h2>Weather Information</h2>
-              <p>City: {catchWeather.name}</p>
-              <p>Temperature: {catchWeather.main && catchWeather.main.temp}°C</p>
-              <p>Weather Condition: {catchWeather.weather && catchWeather.weather[0].description}</p>
-              <p>Feels Like: {catchWeather.main && catchWeather.main.feels_like}°C</p>
-              <p>Wind Speed: {catchWeather.wind && catchWeather.wind.speed} m/s</p>
+              <p>Place: {catchWeather.name}</p>
+              <p>Country: {catchWeather.sys.country}</p>
+              <p>
+                Temperature: {catchWeather.main && catchWeather.main.temp}°C
+              </p>
+              <p>
+                Weather Condition:{" "}
+                {catchWeather.weather && catchWeather.weather[0].description}
+              </p>
+              <p>
+                Feels Like: {catchWeather.main && catchWeather.main.feels_like}
+                °C
+              </p>
+              <p>
+                Wind Speed: {catchWeather.wind && catchWeather.wind.speed} m/s
+              </p>
+              <p>Time: Not implemented</p>
+
+              <img
+                src={`http://openweathermap.org/img/w/${catchWeather.weather[0].icon}.png`}
+                alt="weather icon"
+              />
             </div>
           )}
-
           <div className="bottom-part"></div>
         </div>
         <Search onSearch={handleSearch} />
@@ -113,4 +133,3 @@ const CurrentWeather = () => {
 };
 
 export default CurrentWeather;
-
