f6211833b9d760cd19c03c99d1ae670516540e62
Neda Salahian <neda.salahian@gmail.com>
2024-01-17 14:41:29 +0100

    to merge the remote branch into neda-branch

--- src/component/CurrentWeather.jsx
+++ src/component/CurrentWeather.jsx
@@ -1,18 +1,19 @@
-import React, { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import { data } from "../data/pictureData";
-import "../currentweather.css";
 import Search from "./Search.jsx";
 import { Username } from "../context/Username.jsx";
 import WeatherContext from "../context/WeatherContext.jsx";
+import "../App.css";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ onLogOut }) => {
   const [catchWeather, setCatchWeather] = useState(null);
   const { name } = useContext(Username);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [error, setError] = useState(null);
-  const apiKey = 'd54a96e0b4517e304ec98021394e455b';
-  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  const apiKey = "d54a96e0b4517e304ec98021394e455b";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+  const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
     // Get the user's current location by default
@@ -24,10 +25,12 @@ const CurrentWeather = () => {
       },
       (error) => {
         console.error(`Error getting location: ${error.message}`);
-        setError('Error getting location. Please use the search to enter a city.');
+        setError(
+          "Error getting location. Please use the search to enter a city."
+        );
       }
     );
-  }, []);
+  }, [longitude, latitude]);
 
   const fetchWeatherData = async (lat, lon) => {
     const url = `${apiUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
@@ -44,15 +47,15 @@ const CurrentWeather = () => {
       setError(null);
     } catch (error) {
       console.error(`Error in the request: ${error.message}`);
-      setError('Error fetching weather data.');
+      setError("Error fetching weather data.");
     }
   };
 
   const handleSearch = (city) => {
-    if (city.trim() !== '') {
+    if (city.trim() !== "") {
       fetchWeatherDataByCity(city);
     } else {
-      setError('Please enter a valid city name.');
+      setError("Please enter a valid city name.");
     }
   };
 
@@ -75,42 +78,43 @@ const CurrentWeather = () => {
       }
     } catch (error) {
       console.error(`Error in the request: ${error.message}`);
-      setError('Error fetching weather data.');
+      setError("Error fetching weather data.");
     }
   };
 
   // Bild für das jetzige Wetter
   const weatherBack = `url(${
-    data.find((item) => item.name === "moderate cloudy")?.pic
+    data.find((item) => item.name === catchWeather?.weather?.[0]?.description)
+      ?.pic
   })`;
+  const defaultPic = `src/data/default.jpg`;
   // Bild für das jetzige Wetter
 
+  console.log(catchWeather);
   return (
     <div className="display-weather" style={{ backgroundImage: weatherBack }}>
       <WeatherContext.Provider value={{ catchWeather, setCatchWeather }}>
-        <div className="top-part">
-          <h3>Willkommen {name}. Haben Sie einen schönen Tag</h3>
-
+        <div className="left-part">
+          <h3 className="leftpart-greeting">Hai {name}. Have a good day ! </h3>
           {!catchWeather ? (
-            <p style={{ color: 'black' }}>{error || 'Loading....'}</p>
+            <p style={{ color: "black" }}>{error || "Loading...."}</p>
           ) : (
-            <div>
+            <div className="leftpart-weatherinfo">
               <h2>Weather Information</h2>
               <p>City: {catchWeather.name}</p>
-              <p>Temperature: {catchWeather.main &&  Math.round(parseFloat(catchWeather.main.temp))}°C</p>
+              <p>Temperature: {catchWeather.main && catchWeather.main.temp}°C</p>
               <p>Weather Condition: {catchWeather.weather && catchWeather.weather[0].description}</p>
-              <p>Feels Like: {catchWeather.main && Math.round(parseFloat(catchWeather.main.feels_like))}°C</p>
+              <p>Feels Like: {catchWeather.main && catchWeather.main.feels_like}°C</p>
               <p>Wind Speed: {catchWeather.wind && catchWeather.wind.speed} m/s</p>
             </div>
           )}
-
-          <div className="bottom-part"></div>
         </div>
-        <Search onSearch={handleSearch} />
+        <div className="right-part">
+          <Search onSearch={handleSearch} onLogOut={onLogOut} />
+        </div>
       </WeatherContext.Provider>
     </div>
   );
 };
 
 export default CurrentWeather;
-
--- src/data/pictureData.js
+++ src/data/pictureData.js
@@ -1,22 +1,62 @@
 export const data = [
     {
         name: "sunny",
-        pic: "src/data/pxfuel.jpg"
+        pic: "src/data/sunny.jpg"
+    },
+    {
+        name: "clear sky",
+        pic: "src/data/clear-sky.jpg"
+    },
+    {
+        name: "fog",
+        pic: "src/data/fog.jpg",
+    },
+    {
+        name: "haze",
+        pic: "src/data/haze.jpg"
+    },
+    {
+        name: "mist",
+        pic: "src/data/mist.jpg"
+    },
+    {
+        name: "few clouds",
+        pic: "src/data/few-clouds.jpg"
     },
     {
         name: "partly cloudy",
-        pic: "src/data/StockSnap_X92E0YRPBU.jpg"
+        pic: "src/data/partly-cloudy.jpg"
     },
     {
         name: "moderate cloudy",
-        pic: "src/data/StockSnap_EO03PBJXKL.jpg"
+        pic: "src/data/moderate-cloudy.jpg"
+    },
+    {
+        name: "broken clouds",
+        pic: "src/data/broken-clouds.jpg"
+    },
+    {
+        name: "overcast clouds",
+        pic: "src/data/overcast-cloudy.jpg"
+    },
+    {
+        name: "scattered clouds",
+        pic: "src/data/scattered-clouds.jpg"
     },
     {
         name: "light rain",
-        pic: "src/data/StockSnap_19Z4FEJEJD.jpg",
+        pic: "src/data/light-rain.jpg",
     },
     {
         name: "moderate rain",
-        pic: "src/data/StockSnap_V8BBXLYNJA.jpg",
+        pic: "src/data/moderate-rain.jpg",
+    },
+    {
+        name: "snow",
+        pic: "src/data/snow.jpg",
+    },
+    {
+        name: "light snow",
+        pic: "src/data/light-snow.jpg",
     },
 ];
